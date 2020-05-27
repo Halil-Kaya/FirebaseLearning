02f75730932dfd0d606f4589f51f3f964cc9d63d
Halil-Kaya <hibrahimkayahik@gmail.com>
2020-05-27 04:13:44 +0300

    kameradan ve galeriden resim secme eklendi

--- app/src/main/java/com/halilkaya/firebaseauthentication/HesapAyarlariActivity.kt
+++ app/src/main/java/com/halilkaya/firebaseauthentication/HesapAyarlariActivity.kt
@@ -1,8 +1,13 @@
 package com.halilkaya.firebaseauthentication
 
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
@@ -11,13 +16,35 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.halilkaya.firebaseauthentication.Fragments.ProfilResmiFragment
+import com.halilkaya.firebaseauthentication.Fragments.onProfilResmiListener
 import com.halilkaya.firebaseauthentication.Model.Kullanici
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_hesap_ayarlari.*
 
-class HesapAyarlariActivity : AppCompatActivity() {
+class HesapAyarlariActivity : AppCompatActivity(),onProfilResmiListener {
 
     var kullanici = FirebaseAuth.getInstance().currentUser
 
+    var izinlerVerildiMi:Boolean = false
+
+
+    var galeridenGelenUri:Uri? = null
+    var kameradanGelenBitmap:Bitmap? = null
+
+    override fun getResimYolu(resimPath: Uri?) {
+
+        galeridenGelenUri = resimPath
+        Picasso.get().load(galeridenGelenUri).resize(100,100).into(imgProfilResmi)
+
+    }
+
+    override fun getResimBitmap(bitmap: Bitmap) {
+
+        kameradanGelenBitmap = bitmap
+        imgProfilResmi.setImageBitmap(kameradanGelenBitmap)
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -110,6 +137,64 @@ class HesapAyarlariActivity : AppCompatActivity() {
             }
 
         }
+
+
+
+        imgProfilResmi.setOnClickListener {
+
+            if(izinlerVerildiMi) {
+                var profilResmiFragment = ProfilResmiFragment()
+                profilResmiFragment.show(supportFragmentManager, "frag-ok")
+            }else{
+                izinleriIste()
+            }
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+    fun izinleriIste(){
+
+        var izinler = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
+        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        android.Manifest.permission.CAMERA)
+
+        if(ContextCompat.checkSelfPermission(this,izinler[0]) == PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this,izinler[1]) == PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this,izinler[2]) == PackageManager.PERMISSION_GRANTED){
+
+            izinlerVerildiMi = true
+        }else{
+            ActivityCompat.requestPermissions(this,izinler,150)
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        if(requestCode == 150){
+
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED){
+
+                var profilResmiFragment = ProfilResmiFragment()
+                profilResmiFragment.show(supportFragmentManager, "fotoSec")
+            }else{
+
+                Toast.makeText(this,"tum izinleri vermelisiniz",Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
 
@@ -157,15 +242,6 @@ class HesapAyarlariActivity : AppCompatActivity() {
 
 
     }
-
-
-
-
-
-
-
-
-
 
 
 
